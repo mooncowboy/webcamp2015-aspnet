4828ff2618fbfcc01a552ba685e383082ab2e87e
Ricardo Fiel <ricardo.fiel@rupeal.com>
2015-12-01 09:07:01 +0000

    Upgraded to rc1-upgrade1

---
+++ doc/linux-install.sh
@@ -0,0 +1,26 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+
+sudo apt-get install libunwind8 libssl-dev unzip
+sudo apt-get install mono-devel
+mozroots --import --sync
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+source /home/azureuser/.dnx/dnvm/dnvm.sh
+dnvm upgrade
+
+sudo apt-get install make automake libtool
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
+
+sudo apt-get install git
+
+sudo service unscd stop
+
+sudo apt-get install nginx
